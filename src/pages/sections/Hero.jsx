b13c80e610ab6469a220f7274305028dd5ff2c33
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-19 15:36:37 +0100

    correction

--- src/pages/sections/Hero.jsx
+++ src/pages/sections/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <Navbar />
       <div className="w-full section text-white bg-transparent h-screen backdrop-blur-md flex flex-col justify-center">
         <div className=" w-full">
-          <h1 className=" md:w-1/2 sm:text-[5rem] w-[90%] text-[3.7rem] my-5 sm:my-8 leading-[4.5rem] font-bold capitalize">
+          <h1 className=" md:w-1/2 sm:text-[5rem] w-[95%] text-[3.7rem] my-5 sm:my-8 leading-[4.5rem] font-bold capitalize">
             Experience the difference of professionally trained defensive drivers
           </h1>
           <p className="sm:text-4xl my-6 mb-16 md:w-1/2 w-full text-3xl normal-case text-white font-medium">

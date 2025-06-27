0e1f733269c6e0e0c860ea9054396e337f1210cd
jo-shua09 <joshuaonyeka039@gmail.com>
2025-06-27 12:14:09 +0100

    update package-lock.json and adjust AppLayout component styling

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
-  return (
-    <div className="max-w-[150rem] w-full mx-auto m-auto ">
-      <Outlet />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="max-w-[185rem] w-full mx-auto m-auto ">
+			<Outlet />
+			<Footer />
+		</div>
+	);
 };
 
 export default AppLayout;
